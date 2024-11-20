588f71bb933417f62a51531bbcf294c93ba61367
GomesRojasCarlosManuel <gomezrojascarlosmanuel@gmail.com>
2024-11-19 19:07:05 -0500

    Delete repaso-php directory

--- repaso-php/ejercicio01.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $sueldo=1000
-    // Cálculo del descuento del 9%
-    $descuento = $sueldo * 0.09;
-
-    // Mostrar el resultado con dos decimales usando printf
-    printf("El descuento de EsSalud es: S/ %.2f", $descuento);
-?>
--- repaso-php/index.php
+++
@@ -1,6 +0,0 @@
-<?php
- echo "<h1>hola mundo</h1>";
-
- //<>hola
-//   <>
-?>
